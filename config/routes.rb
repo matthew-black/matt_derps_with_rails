5b90559c33a52bd9426437fd5de3158a6a4afa1c
Matthew Black <matthewallenblack@gmail.com>
2023-07-10 15:17:36 -0500

    add user validations and play with @tofu instance variable

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: redirect("/users")
+  # Or, if we don't want /users to appear in the URL bar:
+  # root "users#index"
 end
